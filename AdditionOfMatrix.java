f2f87b2368a26d64114515e35d962416a8adaf46
Shivam kumar <shivammackbookmaster@Shivams-MacBook-Air.local>
2026-03-22 15:39:17 +0530

    Add AddOddNumbers.java to calculate the sum of odd numbers from 1 to 100

---
+++ AdditionOfMatrix.java
@@ -0,0 +1,73 @@
+/* Addition of Two Matrices 
+
+DECLARE matrixA AS ARRAY OF INTEGER
+SET matrixA := NEW INTEGER[3][3]
+matrixA[0][0] := 1
+matrixA[0][1] := 2
+matrixA[0][2] := 3
+matrixA[1][0] := 4
+matrixA[1][1] := 5
+matrixA[1][2] := 6
+matrixA[2][0] := 7
+matrixA[2][1] := 8
+matrixA[2][2] := 9
+DECLARE matrixB AS ARRAY OF INTEGER
+SET matrixB := NEW INTEGER[3][3]
+matrixB[0][0] := 9
+matrixB[0][1] := 8
+matrixB[0][2] := 7
+matrixB[1][0] := 6
+matrixB[1][1] := 5
+matrixB[1][2] := 4
+matrixB[2][0] := 3
+matrixB[2][1] := 2
+matrixB[2][2] := 1
+DECLARE result AS ARRAY OF INTEGER
+SET result := NEW INTEGER[3][3]
+FOR i FROM 0 TO 2
+    FOR j FROM 0 TO 2
+        result[i][j] := matrixA[i][j] + matrixB[i][j]
+    END FOR
+END FOR
+PRINT "Result of Addition of Matrix A and Matrix B:"
+FOR i FROM 0 TO 2
+    FOR j FROM 0 TO 2
+        PRINT result[i][j] + " "
+    END FOR
+    PRINT NEW LINE
+END FOR
+
+*/
+public class AdditionOfMatrix {
+    public static void main(String[] args) {
+        int[][] matrixA = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int[][] matrixB = {
+            {9, 8, 7},
+            {6, 5, 4},
+            {3, 2, 1}
+        };
+
+        int[][] result = new int[3][3];
+
+        // Adding the two matrices
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                result[i][j] = matrixA[i][j] + matrixB[i][j];
+            }
+        }
+
+        // Printing the result
+        System.out.println("Result of Addition of Matrix A and Matrix B:");
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
